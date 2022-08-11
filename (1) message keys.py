0904cd70526fb12c35326aa04476fd23caadfe37
guochen-code <83231868+guochen-code@users.noreply.github.com>
2022-08-10 23:11:26 -0600

    Update (1) message keys.py

--- (1) message keys.py
+++ (1) message keys.py
@@ -6,3 +6,6 @@
   target partition = Math.abs(Utils.murmur2(keyBytes)))%(numPartition-1)
   
   
+To produce data to a topic, a producer must provide the Kafka client with...
+you only need to connect to one broker (any broker) and just provide the topic name you want to write to. 
+Kafka Clients will route your data to the appropriate brokers and partitions for you!
